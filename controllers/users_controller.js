7e97ca956dd4b90861c9ce90104dbfe15d8a6dc0
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2020-05-08 16:02:35 +0530

    Making all sign in and signup pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,28 @@ module.exports.profile = function(req,res){
         title: "users page"
     })
 }
+
+// Render the sign up page
+module.exports.signup = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+}
+
+// Render the sign in page
+module.exports.signin = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the signup data
+module.exports.create = function(req,res){
+    //TODO later
+
+}
+
+//sign in and create a session 
+module.exports.createsession = function(req,res){
+    //todo later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersControllers = require('../controllers/users_controller');
 
 router.get('/profile',usersControllers.profile);
-
+router.get('/sign-up',usersControllers.signup);
+router.get('/sign-in',usersControllers.signin);
 
 module.exports = router;
