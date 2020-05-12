7a6b3a3dd394287a2d9ee922cd3525170f93fe00
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2020-05-12 13:30:43 +0530

    Deleting comments from the post by user of the post or by who made comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -23,3 +23,21 @@ module.exports.create = function(req,res){
     })
 
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user == req.user.id){
+//saving postid of the deleted comment , so that we can delete that comment also from the comment array inside the post in database
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { 
+                $pull: {
+                    comments: req.params.id
+                 }}, function(err,post){
+                     return res.redirect('back');
+                 })
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,6 @@ const commentsController = require('../controllers/comments_controller');
 
 //only to enter post if user is signed in 2nd level of security
 router.post('/create',passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 module.exports = router;
