07f98e26d633fd3a8f5e22d4fb41ae82a75e567e
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2020-05-17 09:51:55 +0530

    adding picture of user readd after, not done correctly

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+//make the uploads available to the browser
+app.use('/uploads',express.static(__dirname + '/uploads'));
+
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
